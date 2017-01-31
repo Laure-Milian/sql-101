3e8771e97f70692cc884b836d8665d5442a2c286
Laure <laure.milian@gmail.com>
2017-01-31 14:34:29 +0100

    requete preparee

--- index_pdo.php
+++ index_pdo.php
@@ -3,21 +3,35 @@
 $bdd = new PDO(
 	'mysql:host=localhost;dbname=mon_armoire;charset=utf8', 'root', 'simplonco');
 
-$reponse = $bdd->query('SELECT * FROM mes_chaussettes');
+// $reponse = $bdd->query('SELECT * FROM mes_chaussettes');
+
+$req = $bdd->prepare(
+	'SELECT * FROM mes_chaussettes WHERE couleur = ?');
+$req->execute(
+	array('rouge')
+);
 
 ?>
 
 <table>
-<?php while ($donnees = $reponse->fetch()) : ?>
+<tr>
+	<th>Id</th>
+	<th>Couleur</th>
+	<th>Pointure</th>
+	<th>Temp_lavage</th>
+	<th>Description</th>
+	<th>Date_lavage</th>
+</tr>
+<?php while ($donnees = $req->fetch()) : ?>
 		<tr>
 			<td><?=$donnees['id']?></td>
+			<td><?=$donnees['couleur']?></td>
 			<td><?=$donnees['pointure']?></td>
 			<td><?=$donnees['temp_lavage']?></td>
 			<td><?=$donnees['description']?></td>
-			<td><?=$donnees['couleur']?></td>
 			<td><?=$donnees['date_lavage']?></td>
 		</tr>
 <?php endwhile; ?>
 </table>
 
-<?php $reponse->closeCursor(); ?>
+<?php $req->closeCursor(); ?>
